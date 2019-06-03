11c0b24487f164760bd89d4af4e24d133de3e5ba
Jean DEBENEY <jean.debeney@ekimetrics.com>
2019-06-03 14:16:35 +0800

    functions get transactions files list and read from processed data folder

--- data_processing.py
+++ data_processing.py
@@ -8,6 +8,26 @@ import missingno
 # HELPER FUNCTIONS
 #-----------------------------------------------------------------------------------------
 
+TRANSACTIONS_FOLDER = "P:/0. R&D/6. SKU sales forecast/1_Raw data/1_LAN_AAD_data"
+DATA_FOLDER = "P:/0. R&D/6. SKU sales forecast/1_Raw data/2_Processed_Data"
+
+
+# Return all paths to files in the transactions files folder
+def get_transactions_files_list():
+    files = os.listdir(TRANSACTIONS_FOLDER)
+    files = [os.path.join(TRANSACTIONS_FOLDER, file) for file in files]
+    return files
+
+
+def read_from_processed_data_folder(filename, folder = None):
+    if folder is None: folder = DATA_FOLDER
+        path = os.path.join(folder, filename)
+        print(f"... Read file at {path}")
+        if filename.endswith(".csv"):
+            return pd.read_csv(path)
+        else:
+            return pd.read_pickle(path)
+
 
 
 
