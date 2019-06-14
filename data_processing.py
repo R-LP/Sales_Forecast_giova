f07802661ab82c9bd9ae0c5277834efd444df788
Jean DEBENEY <jean.debeney@ekimetrics.com>
2019-06-14 17:49:55 +0800

    function TransactionsProductStore

--- data_processing.py
+++ data_processing.py
@@ -289,11 +289,51 @@ def train_test_split(self, test_size = 0.3, shuffle = False):
     products_train = unique_products[:cut]
     products_test = unique_products[cut:]
     print(f"... Among the {len(unique_products)} : {len(products_train)} in training set and {len(products_test)} in test set")
-    
-     
+
+    # Actually splitting the data
+    train = self.data.loc[self.data["ProductEnglishname"].isin(products_train)]
+    test = self.data.loc[self.data["ProductEnglishname"].isin(products_test)]
+
+    # Retrieving X and y
+    target = "SalesQuantity"
+    X_train, y_train = train.drop(target, axis = 1), train[target]
+    X_test, y_test = test.drop(target, axis = 1), test[target]
+
+    return X_train, y_train, X_test, y_test
 
 
+class TransactionsProductStore(TransactionsAggregated):
+    def __init__(self, data = None):
+        super().__init__(data = data)
+        self.name = "transactions_at_product_store_level"
 
+    def get_data_from_file(self, filepath, keys = None, weekly = True, inplace = True):
+        data = TransactionsMonthlyGranular(filepath)
+        if keys is not None:
+            data.filter_on_products(keys)
+        data = data.groupby_product_store(weekly = weekly)
+        if inplace:
+            self.data = data
+        else:
+            return data
+
+    def get_data_from_files(self, filepaths, keys = None, weekly = True, inplace = True):
+        if isinstance(filepaths, str): filepaths = [filepaths]
+        for i, filepath in enumerate(tqdm_notebook(filepaths)):
+            df = self.get_data_from_file(filepath, keys, weekly, inplace = False)
+            if i == 0:
+                data = df
+            else:
+                data = data.append(df, ignore_index = True)
+
+        if weekly:
+            data = data.groupby(["OrderDate", "ProductEnglishname", "CounterLocalName"], as_index = False).sum()
+        if inplace:
+            self.data = data
+        else:
+            return data
+
+        
 
 
 
