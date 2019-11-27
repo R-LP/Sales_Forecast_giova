d7f92d2e07b650496ec43bcd6bb8989403dbfe84
Jean DEBENEY <jean.debeney@ekimetrics.com>
2019-11-27 15:36:29 +0800

    Adding environment variableS

--- settings.py
+++ settings.py
@@ -4,26 +4,17 @@ import os
 load_dotenv()
 OUTPUT_FOLDER = os.getenv("OUTPUT_FOLDER")
 TRANSACTIONS_FOLDER = os.getenv("TRANSACTIONS_FOLDER")
-epochs = os.getenv("epochs")
-num_layers = os.getenv("num_layers")
-batch_size = os.getenv("batch_size")
-data = os.getenv("data")
-list_products = os.getenv("list_products")
-prediction_length = os.getenv("prediction_length")
-freq = os.getenv("freq")
-min_date = os.getenv("min_date")
-max_date = os.getenv("max_date")
+list_products=os.getenv("list_products")
 
-
-
+# Neural network hyperparameters
 epochs=35
 num_layers=5
 batch_size=50
 
+# data file
 data="LAN.csv"
 
-list_products=["CONF TONIQUE B400ML","GENIFIQUE 13 SERUM B75ML","ADV GEN EYES LIGHT PEARLB20ML"]
-
+# prediction frame
 prediction_length=60
 freq="D"
 min_date="2016-07-01"
